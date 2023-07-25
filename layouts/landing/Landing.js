7e786a7dcc4ecaada6333da7c9834fdf2b50e08c
DiyazMukhanov <Diyaz.mukhanov@gmail.com>
2023-07-25 22:07:58 +0300

    Вёрстка лэндинга прогресс

--- layouts/landing/Landing.js
+++ layouts/landing/Landing.js
@@ -1,11 +1,33 @@
-import { Typography } from "@/components/Typography"
-import { Button } from "../../components/Button/index"
+import styles from './Landing.module.scss'
+import Image from 'next/image'
+import Logo from '../../public/images/Logo.svg'
+import { Typography } from '@/components/Typography'
+import { Button } from '@/components/Button'
 
 export default function Landing() {
     return (
       <>
-      <Button variant="outlined">Test</Button>
-      <Typography element='h2' size='large'>Paragraph</Typography>
+      
+      <header className={styles.header}>
+        <div className={styles.logo}>
+           <Image
+           priority
+           src={Logo}
+           height={44}
+           width={101}
+           />
+        </div>
+        <div className={styles.topButtons}>
+         <Typography element='p' className={styles.enterBtn}>Войти</Typography>
+         <Button variant='outlined' className={styles.registrationBtn}>Регистрация</Button>
+        </div>
+      </header>
+      
+      <main className={styles.main}>
+         <Typography element='h1' className={styles.mainHeading}>Онлайн - платформа для изучения английского языка</Typography>
+         <Button variant='contained' className={styles.startBtn}>Начать обучение</Button>
+         <p className={styles.haveAccountBtn}>У меня уже есть аккаунт</p>
+      </main>
       </>
     )
   }
--- pages/_document.jsx
+++ pages/_document.jsx
@@ -4,9 +4,12 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-      <link rel="preconnect" href="https://fonts.googleapis.com"/>
+      {/* <link rel="preconnect" href="https://fonts.googleapis.com"/>
       <link crossOrigin="true" rel="preconnect" href="https://fonts.gstatic.com"/>
-      <link href="https://fonts.googleapis.com/css2?family=Open+Sans&display=swap" rel="stylesheet"/>
+      <link href="https://fonts.googleapis.com/css2?family=Open+Sans&display=swap" rel="stylesheet"/> */}
+      <link rel="preconnect" href="https://fonts.googleapis.com" />
+      <link crossOrigin="true" rel="preconnect" href="https://fonts.gstatic.com" />
+      <link href="https://fonts.googleapis.com/css2?family=Inter&display=swap" rel="stylesheet" />
         </Head>
       <body>
         <Main />
