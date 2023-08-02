9741502110ed1341ec0d80dfd4c1b51329b03714
DiyazMukhanov <Diyaz.mukhanov@gmail.com>
2023-08-02 16:32:51 +0300

    Level styling completed

--- layouts/Test/Level.jsx
+++ layouts/Test/Level.jsx
@@ -1,5 +1,7 @@
+import { Typography } from '@/components/Typography'
 import styles from './Level.module.scss'
 import Header from '@/components/Header'
+import { Button } from '@/components/Button'
 
 
 
@@ -7,7 +9,14 @@ import Header from '@/components/Header'
 export default function Level() {
     return (
     <>
-      <Header variant='white'></Header>
+      <Header variant='blue'></Header>
+      <div className={styles.container}>
+      <Typography size='small' element='h3' className={styles.text}>Пройдите короткий тест.</Typography>
+      <Typography size='small' element='h3' className={styles.text}>Мы определим ваш уровень.</Typography>
+      <Button variant='authLargeContained' className={styles.levelBtn}>Определить мой уровень</Button>
+      <Button variant='standardLargeOutlined' className={styles.levelBtnTwo}>Я выберу программу самостоятельно</Button>
+      </div>
     </>
     )
 }
+
