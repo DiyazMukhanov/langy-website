b28cc99ff2435bf7ce253c10200bc26ddb418bb1
DiyazMukhanov <Diyaz.mukhanov@gmail.com>
2023-08-03 15:28:28 +0300

    Video layout progress

--- layouts/Lessons/LessonLayout.jsx
+++ layouts/Lessons/LessonLayout.jsx
@@ -1,13 +1,39 @@
 import Header from "@/components/Header";
 import styles from './LessonLayout.module.scss';
 import SideBar from "./SideBar";
+import { useEffect, useRef } from "react";
+import classNames from "classnames";
 
-export default function LessonLayout({ children }) {
+export default function LessonLayout({ children, lessonsSummary }) {
+    const progress = 10
+
+    const currentLesson = lessonsSummary.filter(lesson => lesson.isCurrent === true)
+    const currentChapter = currentLesson[0].lessons.filter(chapter => chapter.isCurrent === true)
+    console.log(currentChapter[0].ru)
+
     return (<>
     <Header variant='white'/>
     <div className={styles.container}>
-        <SideBar />
+        <SideBar lessonsSummary={lessonsSummary}/>
+        <div className={styles.rightSide}>
+            <div className={styles.progressContainer}>
+                <div style={{
+            width: `${progress}%`,
+            height: '100%',
+            background: '#48C61C'
+            }}/>
+            </div>
+         
+        <div className={styles.links}>
+           <p className={classNames({[styles.currentLink]: currentChapter[0].ru === 'Грамматика'})}>Грамматика</p>
+           <p className={classNames({[styles.currentLink]: currentChapter[0].ru === 'Аудио/Чтение'})}>Аудио/Чтение</p>
+           <p className={classNames({[styles.currentLink]: currentChapter[0].ru === 'Письмо'})}>Письмо</p>
+           <p className={classNames({[styles.currentLink]: currentChapter[0].ru === 'Тест'})}>Тест</p>
+        </div>
+        {children}
+        </div>
     </div>
+    
     </>
     )
 }
--- layouts/Lessons/VideoLessonLayout.jsx
+++ layouts/Lessons/VideoLessonLayout.jsx
@@ -1,10 +1,212 @@
-import Header from "@/components/Header";
-import LessonLayout from "./LessonLayout";
+import LessonLayout from "./LessonLayout"
+import ReactPlayer from 'react-player'
+import { useState, useEffect } from "react"
+import styles from './VideoLessonLayout.module.scss'
+import { IoVolumeHigh, IoVolumeLow, IoVolumeMute, IoPlay, IoPause, IoPlayForwardCircleOutline, IoPlayBackCircleOut } from 'react-icons/io5';
 
 export default function VideoLessonLayout() {
+    const [hasWindow, setHasWindow] = useState(false)
+    useEffect(() => {
+        if(typeof window !== 'undefined') {
+            setHasWindow(true);
+        }
+    }, [])
+    
+    const lessonsSummary = [
+        {
+            title: 'Урок 1',
+            lessonNumber: 1,
+            isCompleted: false,
+            isCurrent: true,
+            lessons: [
+                {
+                    chapter: 'Grammar: video lesson',
+                    ru: 'Грамматика',
+                    isCurrent: true,
+                    isCompleted: true
+                },
+                {
+                    chapter: 'Listening/Reading',
+                    ru: 'Аудио/Чтение',
+                    isCurrent: false,
+                    isCompleted: false
+                },
+                {
+                    chapter: 'Writing',
+                    ru: 'Письмо',
+                    isCurrent: false,
+                    isCompleted: false
+                },
+                {
+                    chapter: 'Test',
+                    ru: 'Тест',
+                    isCurrent: false,
+                    isCompleted: false
+                },
+            ]
+        },
+        {
+            title: 'Урок 2',
+            lessonNumber: 2,
+            isCompleted: false,
+            isCurrent: false,
+            lessons: [
+                {
+                    chapter: 'Grammar: video lesson',
+                    ru: 'Грамматика',
+                    isCurrent: false,
+                    isCompleted: true
+                },
+                {
+                    chapter: 'Listening/Reading',
+                    ru: 'Аудио/Чтение',
+                    isCurrent: false,
+                    isCompleted: false
+                },
+                {
+                    chapter: 'Writing',
+                    ru: 'Письмо',
+                    isCurrent: false,
+                    isCompleted: false
+                },
+                {
+                    chapter: 'Test',
+                    ru: 'Тест',
+                    isCurrent: false,
+                    isCompleted: false
+                },
+            ]
+        },
+        {
+            title: 'Урок 3',
+            lessonNumber: 3,
+            isCompleted: false,
+            isCurrent: false,
+            lessons: [
+                {
+                    chapter: 'Grammar: video lesson',
+                    ru: 'Грамматика',
+                    isCurrent: false,
+                    isCompleted: true
+                },
+                {
+                    chapter: 'Listening/Reading',
+                    ru: 'Аудио/Чтение',
+                    isCurrent: false,
+                    isCompleted: false
+                },
+                {
+                    chapter: 'Writing',
+                    ru: 'Письмо',
+                    isCurrent: false,
+                    isCompleted: false
+                },
+                {
+                    chapter: 'Test',
+                    ru: 'Тест',
+                    isCurrent: false,
+                    isCompleted: false
+                },
+            ]
+        },
+        {
+            title: 'Урок 4',
+            lessonNumber: 4,
+            isCompleted: false,
+            isCurrent: false,
+            lessons: [
+                {
+                    chapter: 'Grammar: video lesson',
+                    ru: 'Грамматика',
+                    isCurrent: false,
+                    isCompleted: true
+                },
+                {
+                    chapter: 'Listening/Reading',
+                    ru: 'Аудио/Чтение',
+                    isCurrent: false,
+                    isCompleted: false
+                },
+                {
+                    chapter: 'Writing',
+                    ru: 'Письмо',
+                    isCurrent: false,
+                    isCompleted: false
+                },
+                {
+                    chapter: 'Test',
+                    ru: 'Тест',
+                    isCurrent: false,
+                    isCompleted: false
+                },
+            ]
+        },
+        {
+            title: 'Урок 5',
+            lessonNumber: 5,
+            isCompleted: false,
+            isCurrent: false,
+            lessons: [
+                {
+                    chapter: 'Grammar: video lesson',
+                    ru: 'Грамматика',
+                    isCurrent: false,
+                    isCompleted: true
+                },
+                {
+                    chapter: 'Listening/Reading',
+                    ru: 'Аудио/Чтение',
+                    isCurrent: false,
+                    isCompleted: false
+                },
+                {
+                    chapter: 'Writing',
+                    ru: 'Письмо',
+                    isCurrent: false,
+                    isCompleted: false
+                },
+                {
+                    chapter: 'Test',
+                    ru: 'Тест',
+                    isCurrent: false,
+                    isCompleted: false
+                },
+            ]
+        }
+    ]
+
+    const [playing, setPlaying] = useState(false)
+    const [muted, setMuted] = useState(false)
+    const [volume, setVolume] = useState(0.8)
+
+    const handlePlayPause = () => {
+        setPlaying(!playing);
+      }
+
+    const handleMuteUnmute = () => {
+        setMuted(!muted);
+      } 
+    
+    const handleVolumeChange = (e) => {
+        setVolume(parseFloat(e.target.value));
+      }
+
     return (
-        <LessonLayout>
-            
+        <LessonLayout lessonsSummary={lessonsSummary}>
+            <p>Тема: Present Simple/Настоящее время</p>
+            <p>Посмотрите следующее видео:</p>
+            <div className={styles.wrapper}>
+            {hasWindow && <ReactPlayer
+                url='https://youtu.be/d5KjfW8gBpk'
+                className={styles.player}
+                width="100%"
+                height="100%"
+                playing={playing}
+                volume={volume}
+                muted={muted}
+                controls={true}
+            />}
+            </div>
         </LessonLayout>
     )
 }
