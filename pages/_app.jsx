3ee5c913d2d52345708fa05009b6172cf6617ef5
DiyazMukhanov <Diyaz.mukhanov@gmail.com>
2023-08-04 18:54:21 +0300

    Test lesson progress

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,20 +1,38 @@
 import '../styles/globals.scss'
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import Head from "next/head";
 import { DndProvider } from "react-dnd";
+import { HTML5Backend } from "react-dnd-html5-backend";
 import { TouchBackend } from "react-dnd-touch-backend";
-
+import { isTouchDevice } from "../utils/helpers";
 
 function MyApp({ Component, pageProps }) {
+    const [isClient, setIsClient] = useState(false);
+    const [isTouch, setIsTouch] = useState(false);
+
+    useEffect(() => {
+        setIsClient(true);
+        setIsTouch(isTouchDevice());
+      }, []);
+
+  let dndBackend = HTML5Backend;
+
+  if (isTouchDevice) {
+    dndBackend = TouchBackend;
+  }
+
   return (
       <>
           <Head>
               <title>Langy</title>
               <meta name="описание" content="Уроки английского языка"/>
           </Head>
-      <DndProvider backend={TouchBackend}>    
-        <Component {...pageProps} />
-      </DndProvider>
+          {isClient && (
+            <DndProvider backend={isTouch ? TouchBackend : HTML5Backend}>    
+               <Component {...pageProps} />
+          </DndProvider>
+          )}
+      
       </>
         )
 }
---
+++ utils/helpers.js
@@ -0,0 +1,9 @@
+// helpers.js
+export const isTouchDevice = () => {
+    return (
+      "ontouchstart" in window ||
+      navigator.maxTouchPoints > 0 ||
+      navigator.msMaxTouchPoints > 0
+    );
+  };
+  
