e43e3b23e6ccda1a32c7b5cb6b68932c3797d8ad
DiyazMukhanov <Diyaz.mukhanov@gmail.com>
2023-08-05 09:03:58 +0300

    Test lesson progress

--- layouts/Lessons/TestLessonLayout.jsx
+++ layouts/Lessons/TestLessonLayout.jsx
@@ -1,16 +1,20 @@
 import LessonLayout from "./LessonLayout";
 import { lessonsSummary } from "@/utils/lessonsSummary";
 import styles from './TestLessonLayout.module.scss'
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import classNames from "classnames";
-import { DndProvider, useDrag, useDrop } from "react-dnd";
-import { HTML5Backend } from "react-dnd-html5-backend";
+import NextButton from '../../public/images/Next-button.svg'
+import RightTick from '../../public/images/Right-tick.svg'
+import WrongTick from '../../public/images/Wrong-tick.svg'
+import Image from 'next/image'
+import { Typography } from "@/components/Typography";
 
 const questions = [
     {
         id: 1,
         firstPart: 'You shall',
         secondPart: 'this game',
+        description: 'Глагол быть имеет разнеы формы. Здесь нужно было поставить эту форму, потому что она больше подходит. Продолжай обучение, у тебя всё получится!',
         answers: [
             {
                 id: 1,
@@ -38,6 +42,7 @@ const questions = [
         id: 2,
         firstPart: 'You can',
         secondPart: 'this cake',
+        description: 'Глагол быть имеет разнеы формы. Здесь нужно было поставить эту форму, потому что она больше подходит. Продолжай обучение, у тебя всё получится!',
         answers: [
             {
                 id: 1,
@@ -65,6 +70,7 @@ const questions = [
         id: 3,
         firstPart: 'You can',
         secondPart: 'this cat',
+        description: 'Глагол быть имеет разнеы формы. Здесь нужно было поставить эту форму, потому что она больше подходит. Продолжай обучение, у тебя всё получится!',
         answers: [
             {
                 id: 1,
@@ -90,20 +96,80 @@ const questions = [
     }
 ]
 
-const WordContainer = ({ word, isCorrect }) => {
-    const [{ isDragging }, dragRef] = useDrag({
-        type: "WORD_CONTAINER",
-        item: { word, isCorrect },
-        collect: (monitor) => ({
-          isDragging: monitor.isDragging(),
-        }),
-      });
+const ResultsShowing = ({rightAnswers, totalQuestions, questionsArr }) => {
+    const checkIsRight = (question, chosenAnswer) => {
+        const trueAnswer = question.answers.filter(answer => answer.isCorrect === true)
+        const trueResult = trueAnswer[0].answer
+        return trueResult === chosenAnswer
+    }
 
+    const findRightAnswer = (question) => {
+        const trueAnswer = question.answers.filter(answer => answer.isCorrect === true)
+        return trueAnswer[0].answer
+    }
+
+    return (
+    <div className={styles.resultsContainer}>
+        <Typography size='small' element='h3'>Поздравляем!</Typography>
+        <p className={styles.resultText}>Ваш результат: {rightAnswers}/{totalQuestions}</p>
+        <div className={styles.resultsShowing}>
+           {questionsArr.map(question => (
+            <>
+            <div className={styles.answerBlockTop}>
+            <div className={styles.answersAllContainer}>
+                
+                <div className={styles.answerResult}>
+                {checkIsRight(question, question.chosenAnswer) ? 
+                <Image
+                        priority
+                        src={RightTick}
+                    /> : 
+                    <Image
+                    priority
+                    src={WrongTick}
+                />}
+                <p>{question.id}.</p>
+                <p>{question.firstPart}</p>
+                <div className={classNames(
+                    {[styles.resultWordCardRight] : checkIsRight(question, question.chosenAnswer)},
+                    {[styles.resultWordCardWrong] : !checkIsRight(question, question.chosenAnswer)},
+                    )}>{question.chosenAnswer}</div>
+                <p>{question.secondPart}</p>    
+                </div>
+                {!checkIsRight(question, question.chosenAnswer) &&  
+                // if the answer was wrong we show the right answer to user
+                <div className={styles.answerResult}>
+                
+                <Image
+                        priority
+                        src={RightTick}
+                    /> 
+                    
+                <p>{question.firstPart}</p>
+                <div className={styles.resultWordCardRight}>{findRightAnswer(question)}</div>
+                <p>{question.secondPart}</p>
+                </div>
+                }
+                </div>
+            </div>
+            <div></div></>
+           ))}
+        </div>
+        </div>
+        )
+}
+
+const WordContainer = ({ word, isCorrect, onClick, droppedWord }) => {
+    
    return (
    <div 
-        ref={dragRef}
-        className={styles.wordContainer}
-        draggable='true'
+        className={classNames(
+            styles.wordContainer,
+            {[styles.invisible]: droppedWord === word},
+            {[styles.disabled]: droppedWord}
+            )}    
+        onClick={onClick}
+        
         >
       {word}
    </div>
@@ -111,58 +177,104 @@ const WordContainer = ({ word, isCorrect }) => {
 }
 
 export default function TestLessonLayout() {
+    const [questionsArr, setQuestionsArray] = useState(questions)
     const [currentQuestion, setCurrentQuestion] = useState(1)
     const totalQuestions = questions.length
-    const [isCorrect, setIsCorrect] = useState('neutral')
     const [droppedWord, setDroppedWord] = useState(null)
+    const [results, setResults] = useState([])
+    const [isCompleted, setIsCompleted] = useState(false)
 
-    const handleDrop = (word, isCorrect) => {
+    console.log(questionsArr)
+
+    const rightAnswersFind = (arr) => {
+        const onlyTrueArr = arr.filter(item => item === true)
+        return onlyTrueArr.length
+    }
+
+    const rightAnswers = rightAnswersFind(results)
+
+    const handleChoose = (word, isCorrect) => {
         setDroppedWord(word);
-        setIsCorrect(isCorrect ? 'correct' : 'mistake');
-      };
-    
-      const [, dropRef] = useDrop({
-        accept: "WORD_CONTAINER",
-        drop: (item) => handleDrop(item.word, item.isCorrect),
-      });
+        const updatedResults = [...results, isCorrect]
+        setResults(updatedResults)
+        const updatedQuestionsArr = questionsArr.map(item => 
+            item.id === currentQuestion ? {...item, chosenAnswer: word} : item
+            )
 
+         setQuestionsArray(updatedQuestionsArr)   
+      };
+
+      useEffect(() => {
+         setDroppedWord(null)
+      }, [currentQuestion])
+
+      const nextHandler = () => {
+        if(currentQuestion < totalQuestions) {
+            if(!droppedWord) {
+                alert('Выберите вариант ответа')
+                return
+            }
+            setCurrentQuestion(currentQuestion + 1)
+        }
+
+        if(currentQuestion === totalQuestions) {
+            setIsCompleted(true)
+        }
+      }
+    
      return (
      <>
      
        <LessonLayout lessonsSummary={lessonsSummary} chapter='test'>
-         <div className={styles.questionContainer}>
+        {!isCompleted ? (
+            <div className={styles.questionContainer}>
             <div className={styles.top}>
             <p className={styles.questionTitle}>Вопрос {currentQuestion}</p>
             <p>{currentQuestion}/{totalQuestions}</p>
             </div>
 
             <div className={styles.middle}>
-               {questions[currentQuestion - 1].firstPart}
-               <div 
-               className={classNames(
+                {questions[currentQuestion - 1].firstPart}
+                <div 
+                className={classNames(
                 styles.dropZone,
-                {[styles.green]: isCorrect === 'correct'},
-                {[styles.red]: isCorrect === 'mistake'},
+                {[styles.blue]: droppedWord},
                 )}
-               ref={dropRef}
-               onDragOver={(event) => event.preventDefault()}
-               >
+                >
                 {droppedWord && droppedWord}
                 </div>
-               {questions[currentQuestion - 1].secondPart}
+                {questions[currentQuestion - 1].secondPart}
             </div>
 
             <div className={styles.bottom}>
-               {questions[currentQuestion - 1].answers.map(answer => (
+                {questions[currentQuestion - 1].answers.map(answer => (
                 <WordContainer 
                 key={answer.id}
                 word={answer.answer}
                 isCorrect={answer.isCorrect}
+                onClick={() => handleChoose(answer.answer, answer.isCorrect)}
+                droppedWord={droppedWord}
                 />
-               ))}
+                ))}
             </div>
-            
-         </div>
+
+            <div className={styles.buttonsContainer}>
+                {/* <Image
+                    priority
+                    src={PreviousButton}
+                /> */}
+                <Image
+                    priority
+                    src={NextButton}
+                    className={styles.next}
+                    onClick={nextHandler}
+                />
+            </div>
+            </div>
+        ) : (
+            <ResultsShowing rightAnswers={rightAnswers} totalQuestions={totalQuestions} questionsArr={questionsArr} />
+        )}
+        
        </LessonLayout>
        
      </>
--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,41 +1,16 @@
 import '../styles/globals.scss'
-import React, {useState, useEffect} from 'react'
+import React from 'react'
 import Head from "next/head";
-import { DndProvider } from "react-dnd";
-import { HTML5Backend } from "react-dnd-html5-backend";
-import { TouchBackend } from "react-dnd-touch-backend";
-import { isTouchDevice } from "../utils/helpers";
-import TouchDragLayer from '@/utils/TouchDragLayer';
 
 function MyApp({ Component, pageProps }) {
-    const [isClient, setIsClient] = useState(false);
-    const [isTouch, setIsTouch] = useState(false);
-
-    useEffect(() => {
-        setIsClient(true);
-        setIsTouch(isTouchDevice());
-      }, []);
-
-  let dndBackend = HTML5Backend;
-
-  if (isTouch) {
-    dndBackend = TouchBackend;
-  }
-
+ 
   return (
       <>
           <Head>
               <title>Langy</title>
               <meta name="описание" content="Уроки английского языка"/>
-          </Head>
-          {isClient && (
-            <DndProvider backend={isTouch ? TouchBackend : HTML5Backend}>
-                {/* Render the TouchDragLayer to show the moving element */}
-               {isTouch && <TouchDragLayer />}    
+          </Head>   
                <Component {...pageProps} />
-          </DndProvider>
-          )}
-      
       </>
         )
 }
