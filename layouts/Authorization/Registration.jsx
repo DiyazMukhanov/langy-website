7ecc64884be479cd274638eb5313f16c39b7f467
DiyazMukhanov <Diyaz.mukhanov@gmail.com>
2023-08-07 09:28:02 +0300

    Writing lesson progress

--- layouts/Authorization/Registration.jsx
+++ layouts/Authorization/Registration.jsx
@@ -4,8 +4,80 @@ import Google from '../../public/images/Google.svg'
 import Image from 'next/image'
 import { Button } from '@/components/Button'
 import { Typography } from '@/components/Typography'
+// import { googleRegistration } from '../../api/auth'
+import axios from 'axios';
+import { useState } from 'react'
+import { useRouter } from "next/router"
 
 export default function Registration() {
+    const [nameInputValue, setNameInputValue] = useState('')
+    const [emailInputValue, setEmailInputValue] = useState('')
+    const [passwordInputValue, setPasswordInputValue] = useState('')
+    const [passwordConfirmInputValue, setPasswordConfirmInputValue] = useState('')
+
+    // const handleGoogleAuth = async () => {
+    //     try {
+    //       const response = await axios.get('http://localhost:3002/auth/google');
+    //       // Handle the response from the backend, if needed
+    //       console.log('Google Auth Response:', response.data);
+    //     } catch (error) {
+    //       console.error('Google Auth Error:', error);
+    //     }
+    //   };
+
+    const router = useRouter()
+
+      const nameNameInputHandler = (event) => {
+        setNameInputValue(event.target.value)
+      }
+
+      const emailInputHandler = (event) => {
+        setEmailInputValue(event.target.value)
+      }
+
+      const passwordInputHandler = (event) => {
+        setPasswordInputValue(event.target.value)
+      }
+
+      const passwordConfirmInputHandler = (event) => {
+        setPasswordConfirmInputValue(event.target.value)
+      }
+
+      const userSignUpHandler = async () => {
+        const url = 'http://localhost:3000/api/v1/users/signup'; 
+
+        const body = {
+            name: nameInputValue,
+            email: emailInputValue,
+            password: passwordInputValue,
+            passwordConfirm: passwordConfirmInputValue
+        }
+        
+        try {
+            const response = await fetch(url, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                         },
+                body: JSON.stringify(body),
+            })
+    
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+              }
+    
+              const responseData = await response.json();
+              console.log('Response data:', responseData);
+        } catch (error) {
+            console.error('Error:', error);
+            // Handle any errors that occurred during the fetch request
+          }
+      }
+
+      const goToLoginHandler = () => {
+        router.push('/authorization/login')
+      }
+
     return (
     <div className={styles.container}>
         <div className={styles.header}>
@@ -15,22 +87,23 @@ export default function Registration() {
            width={15}
            className={styles.close}
            />
-           <Button variant='standardAuthOutlined'>Войти</Button>
+           <Button variant='standardAuthOutlined' onClick={goToLoginHandler}>Войти</Button>
         </div>
 
         <div className={styles.formContainer}>
            <Typography size='small' element='h2' className={styles.formHeading}>Создайте аккаунт</Typography>
         
         <div className={styles.inputs}>
-          <input placeholder='Имя' className={styles.input}></input>
-          <input placeholder='Электронная почта' className={styles.input}></input>
-          <input placeholder='Пароль' className={styles.input}></input>
-          <input placeholder='Повторить пароль' className={styles.input}></input>
+          <input placeholder='Имя' className={styles.input} onChange={nameNameInputHandler}></input>
+          <input placeholder='Электронная почта' className={styles.input} onChange={emailInputHandler}></input>
+          <input placeholder='Пароль' className={styles.input} onChange={passwordInputHandler}></input>
+          <input placeholder='Повторить пароль' className={styles.input} onChange={passwordConfirmInputHandler}></input>
         </div>
-        <Button variant='authLargeContained'>Зарегистрироваться</Button>
+        <Button variant='authLargeContained' onClick={userSignUpHandler}>Зарегистрироваться</Button>
         <p>или</p>
         <Button 
         variant='google'
+        // onClick={handleGoogleAuth}
         >
            <Image
            priority
--- layouts/Lessons/WritingLessonLayout.jsx
+++ layouts/Lessons/WritingLessonLayout.jsx
@@ -2,30 +2,73 @@ import { Button } from '@/components/Button'
 import LessonLayout from './LessonLayout'
 import styles from './WritingLessonLayout.module.scss'
 import { lessonsSummary } from '@/utils/lessonsSummary'
+import axios from 'axios'
+import { useState } from 'react'
 
 export default function WritingLessonLayout()  {
-    return (
-    <>
-    <LessonLayout lessonsSummary={lessonsSummary} chapter='writing'>
-       <div className={styles.lessonContainer}>
-          <p className={styles.title}>
-             Напишите короткое сочинение на тему “Мое хобби”
-          </p>
-          <textarea
-          type='text'
-          placeholder='Введите текст'
-          className={styles.input}
-          />
-          <div className={styles.checkButtonContainer}>
-            <Button variant='standardNextContained'>Проверить</Button>
-          </div>
-          <p className={styles.title}>Комментарии к сочинению</p>
-          <div className={styles.commentsContainer}></div>
-          <div className={styles.nextButtonContainer}>
-            <Button variant='standardNextContained'>Далее</Button>
-          </div>
-       </div>
-    </LessonLayout>
-    </>
-    )
+    const [essay, setEssay] = useState('')
+    const [response, setResponse] = useState(null)
+    const [isLoading, setIsLoading] = useState(false)
+    console.log(response)
+
+    //Test Chat GPT
+    const handleSubmit = async () => {
+        try {
+            if(essay.length < 1) {
+                alert('Пожалуйста напишите эссе')
+                return
+            }
+
+            setIsLoading(true)
+
+            const headers = {
+                'Authorization': `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY0ZDA2ZDc0MDdlNjNjMDk5NGRkNTBkNCIsImlhdCI6MTY5MTM4OTExNiwiZXhwIjoxNjk5MTY1MTE2fQ.V_87nGcuk10ymHQo2h_Z7kZwu2mxTIoNKMDg9JuHnrw`,
+                'Content-Type': 'application/json', // Set the content type of the request (optional)
+              };
+            const response = await axios.post('https://langy-api.onrender.com/api/v1/users/essay', { essay }, { headers });
+            if(response) {
+                setIsLoading(false)
+            }
+            setResponse(response.data);
+          } catch (error) {
+            console.error('Error making API request:', error);
+            setIsLoading(false)
+            // Handle the error, show an error message, etc.
+          }
+    }
+
+    const inputValueHandler = (event) => {
+        setEssay(event.target.value)
+    }
+    
+    if(isLoading) {
+        return <div>Loading...</div>
+    } else {
+        return (
+            <>
+            <LessonLayout lessonsSummary={lessonsSummary} chapter='writing'>
+               <div className={styles.lessonContainer}>
+                  <p className={styles.title}>
+                     Напишите короткое сочинение на тему “Мое хобби”
+                  </p>
+                  <textarea
+                  type='text'
+                  placeholder='Введите текст'
+                  className={styles.input}
+                  onChange={inputValueHandler}
+                  />
+                  <div className={styles.checkButtonContainer}>
+                    <Button variant='standardNextContained' onClick={handleSubmit}>Проверить</Button>
+                  </div>
+                  <p className={styles.title}>Комментарии к сочинению</p>
+                  <textarea className={styles.commentsContainer} value={response && response.result}>{}</textarea>
+                  <div className={styles.nextButtonContainer}>
+                    <Button variant='standardNextContained'>Далее</Button>
+                  </div>
+               </div>
+            </LessonLayout>
+            </>
+            )
+    }
+  
 }
