255477338ecd9b1f304f3aa64ae0391c3a280396
Дияз Дияз <diyaz.mukhanov@gmail.com>
2023-08-14 07:39:20 +0400

    Completed profile

--- layouts/Profile/Profile.jsx
+++ layouts/Profile/Profile.jsx
@@ -1,3 +1,33 @@
+import Header from "@/components/Header"
+import styles from './/Profile.module.scss'
+import SubscriptionCard from "./SubscriptionCard"
+import ProtectPage from "@/components/ProtectPage"
+import { UserContext } from "@/store/userContext"
+import { useContext } from "react"
+import { useRouter } from "next/router"
+
 export default function Profile() {
-    return <>Личный кабинет</>
+    const userCtx = useContext(UserContext)
+    const router = useRouter()
+
+    const capitalizeFirst = (str) => {
+        if(str) {
+            return str.charAt(0).toUpperCase() + str.slice(1)
+        }
+    }
+
+    return (
+      <ProtectPage>
+        <Header variant='blue'/>
+        <div className={styles.profileContainer}>
+            <div className={styles.leftSide}>
+                <p className={styles.name}>{userCtx?.userData?.name}</p>
+                <h2>Мой уровень</h2>
+                <div className={styles.level}>{capitalizeFirst(userCtx?.userData?.level)}</div>
+                <p className={styles.service} onClick={() => router.push('/service')}>Написать в службу поддержки</p>
+            </div>
+            <SubscriptionCard userData={userCtx?.userData}/>
+        </div>
+      </ProtectPage>
+    )
 }
