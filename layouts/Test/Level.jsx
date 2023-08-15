2cb51132b193db854a9363ffe790520c440811b8
Дияз Дияз <diyaz.mukhanov@gmail.com>
2023-08-15 15:01:26 +0400

    Level completed

--- layouts/Test/Level.jsx
+++ layouts/Test/Level.jsx
@@ -4,13 +4,50 @@ import Header from '@/components/Header'
 import { Button } from '@/components/Button'
 import Loader from '@/components/Loader'
 import { useRouter } from 'next/router'
+import { useContext, useState, useEffect } from 'react'
+import { UserContext } from '@/store/userContext'
+import ProtectPage from '@/components/ProtectPage'
 
 export default function Level() {
   
   const router = useRouter()
+  const userCtx = useContext(UserContext)
+    
+    const [isLoading, setIsLoading] = useState(true)
+
+    useEffect(() => {
+       
+       if(userCtx?.userData?.levelChecked === true) {
+        console.log('level checked')
+        if(userCtx?.userData?.currentLesson !== 0 && userCtx?.userData?.currentChapter !== 'no') {
+            router.push(`/lessons/lesson${userCtx?.userData?.currentLesson}/${userCtx?.userData?.currentChapter}`)
+            setIsLoading(false)
+          } else {
+            if(userCtx?.userData?.level === 'preIntermediate') {
+              router.push('/lessons/lesson9/video')
+              console.log('preIntermediate ok')
+              setIsLoading(false)
+            }
+
+            if(userCtx?.userData?.level === 'intermediate') {
+              router.push('/lessons/lesson17/video')
+              console.log('intermediate ok')
+              setIsLoading(false)
+            }
+
+            if(userCtx?.userData?.level === 'elementary') {
+              router.push('/lessons/lesson1/video')
+              console.log('elementary ok')
+              setIsLoading(false)
+            }
+          }
+       } else {
+        setIsLoading(false)
+       }
+    }, [userCtx.userData])
   
       return (
-        <>
+        <ProtectPage>
           <Header variant='blue'></Header>
           <div className={styles.container}>
           <Typography size='small' element='h3' className={styles.text}>Пройдите короткий тест.</Typography>
@@ -18,7 +55,7 @@ export default function Level() {
           <Button variant='authLargeContained' className={styles.levelBtn} onClick={() => router.push('/test/test')}>Определить мой уровень</Button>
           <Button variant='standardLargeOutlined' className={styles.levelBtnTwo} onClick={() => router.push('/test/choice')}>Я выберу программу самостоятельно</Button>
           </div>
-        </>
+        </ProtectPage>
         )
     }
   
