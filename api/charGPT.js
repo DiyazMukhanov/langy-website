6ecf8b8e7f6061faa42c22f9670b7e898988b01c
DIYAZ MUKHANOV <diyaz.mukhanov@gmail.com>
2023-08-20 06:27:30 +0400

    Prep for server

--- api/charGPT.js
+++ api/charGPT.js
@@ -1,22 +1,24 @@
 import axios from 'axios';
 
 // const apiUrl = 'https://langy-api.onrender.com/api/v1';
-const apiUrl = 'https://langy-api.onrender.com/api/v1';
+const apiUrl = 'http://185.164.173.181:3000/api/v1';
 
-const token = () => {
-    let storedToken
-    if(typeof window !== 'undefined') {
-        storedToken = localStorage.getItem('token')
-    }
-    return storedToken
-  }
+// const token = () => {
+//     let storedToken
+//     if(typeof window !== 'undefined') {
+//         storedToken = localStorage.getItem('token')
+//     }
+//     return storedToken
+//   }
+
+const options = { withCredentials: true, headers: { 'Content-Type': 'application/json'}}
 
   export const sendAssay = async (essay) => {
-    const storedToken = token()
-    const headers = {
-      'Authorization': `Bearer ${storedToken}`,
-      'Content-Type': 'application/json', 
-    };
+    // const storedToken = token()
+    // const headers = {
+    //   'Authorization': `Bearer ${storedToken}`,
+    //   'Content-Type': 'application/json', 
+    // };
   
-    return await axios.post(`${apiUrl}/users/essay`, { essay }, { headers });
+    return await axios.post(`${apiUrl}/users/essay`, { essay }, options);
    } 
