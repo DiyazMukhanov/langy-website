35e5957f0f0e1c4cbff16cb0edac558c88b51a59
DIYAZ MUKHANOV <diyaz.mukhanov@gmail.com>
2023-08-27 10:43:29 +0400

    Lesson 12 completed

---
+++ pages/lessons/lesson12/audio.js
@@ -0,0 +1,1 @@
+export { default } from '../../../lessons/lesson12/audio'
---
+++ utils/lessonsMaterials/lesson12/test-materials.js
@@ -0,0 +1,232 @@
+export const questions = [
+    {
+        id: 1,
+        firstPart: "",
+        secondPart: 'many stars in the sky',
+        description: 'Нужно добавить объяснение',
+        answers: [
+            {
+                id: 1,
+                answer: 'There are',
+                isCorrect: true
+            },
+            {
+                id: 2,
+                answer: 'is there',
+                isCorrect: false
+            },
+            {
+                id: 3,
+                answer: 'There is',
+                isCorrect: false
+            }
+        ] 
+    },
+    {
+        id: 2,
+        firstPart: "",
+        secondPart: 'any sugar?',
+        description: 'Нужно добавить объяснение',
+        answers: [
+            {
+                id: 1,
+                answer: 'There is',
+                isCorrect: false
+            },
+            {
+                id: 2,
+                answer: 'Are there',
+                isCorrect: false
+            },
+            {
+                id: 3,
+                answer: 'Is there',
+                isCorrect: true
+            }
+        ] 
+    },
+    {
+        id: 3,
+        firstPart: '',
+        secondPart: 'any frogs around the pond',
+        description: 'Нужно добавить объяснение',
+        answers: [
+            {
+                id: 1,
+                answer: "There is",
+                isCorrect: false
+            },
+            {
+                id: 2,
+                answer: "There are",
+                isCorrect: false
+            },
+            {
+                id: 3,
+                answer: "There are not",
+                isCorrect: true
+            }
+        ] 
+    },
+    {
+        id: 4,
+        firstPart: '',
+        secondPart: 'any snow',
+        description: 'Нужно добавить объяснение',
+        answers: [
+            {
+                id: 1,
+                answer: 'There is',
+                isCorrect: false
+            },
+            {
+                id: 2,
+                answer: "There isn't",
+                isCorrect: true
+            },
+            {
+                id: 3,
+                answer: "Is there",
+                isCorrect: false
+            }
+        ] 
+    },
+    {
+        id: 5,
+        firstPart: "",
+        secondPart: 'any monkeys in this wood?',
+        description: 'Нужно добавить объяснение',
+        answers: [
+            {
+                id: 1,
+                answer: 'Are there',
+                isCorrect: true
+            },
+            {
+                id: 2,
+                answer: 'There are',
+                isCorrect: false
+            },
+            {
+                id: 3,
+                answer: 'Is there',
+                isCorrect: false
+            }
+        ] 
+    },
+    {
+        id: 6,
+        firstPart: '',
+        secondPart: 'much time',
+        description: 'Нужно добавить объяснение',
+        answers: [
+            {
+                id: 1,
+                answer: "There aren't",
+                isCorrect: false
+            },
+            {
+                id: 2,
+                answer: 'There are',
+                isCorrect: false
+            },
+            {
+                id: 3,
+                answer: "There isn't",
+                isCorrect: true
+            }
+        ] 
+    },
+    {
+        id: 7,
+        firstPart: '',
+        secondPart: 'hundreds of bees',
+        description: 'Нужно добавить объяснение',
+        answers: [
+            {
+                id: 1,
+                answer: "There isn't",
+                isCorrect: false
+            },
+            {
+                id: 2,
+                answer: "Are there",
+                isCorrect: false
+            },
+            {
+                id: 3,
+                answer: "There are",
+                isCorrect: true
+            }
+        ] 
+    },
+    {
+        id: 8,
+        firstPart: "",
+        secondPart: 'any life on Jupiter?',
+        description: 'Нужно добавить объяснение',
+        answers: [
+            {
+                id: 1,
+                answer: 'Is there',
+                isCorrect: true
+            },
+            {
+                id: 2,
+                answer: "There isn't",
+                isCorrect: false
+            },
+            {
+                id: 3,
+                answer: 'There is',
+                isCorrect: false
+            }
+        ] 
+    },
+    {
+        id: 9,
+        firstPart: "",
+        secondPart: 'enough chairs for everyone?',
+        description: 'Нужно добавить объяснение',
+        answers: [
+            {
+                id: 1,
+                answer: "There aren't",
+                isCorrect: false
+            },
+            {
+                id: 2,
+                answer: 'Are there ',
+                isCorrect: true
+            },
+            {
+                id: 3,
+                answer: 'There are',
+                isCorrect: false
+            }
+        ] 
+    },
+    {
+        id: 10,
+        firstPart: "",
+        secondPart: 'many birds in this tree',
+        description: 'Нужно добавить объяснение',
+        answers: [
+            {
+                id: 1,
+                answer: "There aren't",
+                isCorrect: true
+            },
+            {
+                id: 2,
+                answer: "There isn't",
+                isCorrect: false
+            },
+            {
+                id: 3,
+                answer: "Are there",
+                isCorrect: false
+            }
+        ] 
+    }
+]
