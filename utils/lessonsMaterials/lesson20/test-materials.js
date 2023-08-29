e4cdc1e8b74818a42b9c73114743c22ed09fd004
DIYAZ MUKHANOV <diyaz.mukhanov@gmail.com>
2023-08-29 21:24:46 +0400

    Lesson 21 test

---
+++ utils/lessonsMaterials/lesson20/test-materials.js
@@ -0,0 +1,232 @@
+export const questions = [
+    {
+        id: 1,
+        firstPart: "Hi, Chris",
+        secondPart: 'is my friend Jona.',
+        description: 'Нужно добавить объяснение',
+        answers: [
+            {
+                id: 1,
+                answer: 'it',
+                isCorrect: false
+            },
+            {
+                id: 2,
+                answer: 'this',
+                isCorrect: true
+            },
+            {
+                id: 3,
+                answer: 'that',
+                isCorrect: false
+            }
+        ] 
+    },
+    {
+        id: 2,
+        firstPart: "Look at",
+        secondPart: 'cloud. It is very strange.',
+        description: 'Нужно добавить объяснение',
+        answers: [
+            {
+                id: 1,
+                answer: 'it',
+                isCorrect: false
+            },
+            {
+                id: 2,
+                answer: 'this',
+                isCorrect: true
+            },
+            {
+                id: 3,
+                answer: 'that',
+                isCorrect: false
+            }
+        ] 
+    },
+    {
+        id: 3,
+        firstPart: '',
+        secondPart: 'is probably out of battery',
+        description: 'Нужно добавить объяснение',
+        answers: [
+            {
+                id: 1,
+                answer: "this",
+                isCorrect: false
+            },
+            {
+                id: 2,
+                answer: "it",
+                isCorrect: true
+            },
+            {
+                id: 3,
+                answer: "these",
+                isCorrect: false
+            }
+        ] 
+    },
+    {
+        id: 4,
+        firstPart: '',
+        secondPart: 'is a beautiful day',
+        description: 'Нужно добавить объяснение',
+        answers: [
+            {
+                id: 1,
+                answer: 'this',
+                isCorrect: false
+            },
+            {
+                id: 2,
+                answer: "it",
+                isCorrect: true
+            },
+            {
+                id: 3,
+                answer: "that",
+                isCorrect: false
+            }
+        ] 
+    },
+    {
+        id: 5,
+        firstPart: "",
+        secondPart: 'turned out really well',
+        description: 'Нужно добавить объяснение',
+        answers: [
+            {
+                id: 1,
+                answer: 'this',
+                isCorrect: false
+            },
+            {
+                id: 2,
+                answer: 'it',
+                isCorrect: true
+            },
+            {
+                id: 3,
+                answer: 'that',
+                isCorrect: false
+            }
+        ] 
+    },
+    {
+        id: 6,
+        firstPart: "I can't believe",
+        secondPart: "is already time to celebrate your wedding",
+        description: 'Нужно добавить объяснение',
+        answers: [
+            {
+                id: 1,
+                answer: "this",
+                isCorrect: false
+            },
+            {
+                id: 2,
+                answer: 'it',
+                isCorrect: true
+            },
+            {
+                id: 3,
+                answer: "that",
+                isCorrect: false
+            }
+        ] 
+    },
+    {
+        id: 7,
+        firstPart: 'Have you ever tried the cake?',
+        secondPart: 'tastes incredible.',
+        description: 'Нужно добавить объяснение',
+        answers: [
+            {
+                id: 1,
+                answer: "this",
+                isCorrect: false
+            },
+            {
+                id: 2,
+                answer: "it",
+                isCorrect: true
+            },
+            {
+                id: 3,
+                answer: "that",
+                isCorrect: false
+            }
+        ] 
+    },
+    {
+        id: 8,
+        firstPart: "",
+        secondPart: "is imazing how technology has advanced in the last decade",
+        description: 'Нужно добавить объяснение',
+        answers: [
+            {
+                id: 1,
+                answer: 'this',
+                isCorrect: false
+            },
+            {
+                id: 2,
+                answer: "it",
+                isCorrect: true
+            },
+            {
+                id: 3,
+                answer: 'that',
+                isCorrect: false
+            }
+        ] 
+    },
+    {
+        id: 9,
+        firstPart: "The sun is setting and",
+        secondPart: 'creates a beautiful view over the horizon',
+        description: 'Нужно добавить объяснение',
+        answers: [
+            {
+                id: 1,
+                answer: "this",
+                isCorrect: false
+            },
+            {
+                id: 2,
+                answer: 'it',
+                isCorrect: true
+            },
+            {
+                id: 3,
+                answer: 'these',
+                isCorrect: false
+            }
+        ] 
+    },
+    {
+        id: 10,
+        firstPart: "I can't believe you're still using",
+        secondPart: 'oudated computer',
+        description: 'Нужно добавить объяснение',
+        answers: [
+            {
+                id: 1,
+                answer: "this",
+                isCorrect: true
+            },
+            {
+                id: 2,
+                answer: "it",
+                isCorrect: false
+            },
+            {
+                id: 3,
+                answer: "that",
+                isCorrect: false
+            }
+        ] 
+    }
+]
---
+++ utils/lessonsMaterials/lesson21/test-materials.js
@@ -0,0 +1,232 @@
+export const questions = [
+    {
+        id: 1,
+        firstPart: "They",
+        secondPart: 'start a new busuness next year.',
+        description: 'Нужно добавить объяснение',
+        answers: [
+            {
+                id: 1,
+                answer: 'are',
+                isCorrect: false
+            },
+            {
+                id: 2,
+                answer: 'go',
+                isCorrect: false
+            },
+            {
+                id: 3,
+                answer: 'going to',
+                isCorrect: true
+            }
+        ] 
+    },
+    {
+        id: 2,
+        firstPart: "",
+        secondPart: 'come to the party on Sunday?',
+        description: 'Нужно добавить объяснение',
+        answers: [
+            {
+                id: 1,
+                answer: 'are',
+                isCorrect: false
+            },
+            {
+                id: 2,
+                answer: 'will',
+                isCorrect: true
+            },
+            {
+                id: 3,
+                answer: 'is',
+                isCorrect: false
+            }
+        ] 
+    },
+    {
+        id: 3,
+        firstPart: 'We',
+        secondPart: 'have a picnic if the weather is nice',
+        description: 'Нужно добавить объяснение',
+        answers: [
+            {
+                id: 1,
+                answer: "are",
+                isCorrect: false
+            },
+            {
+                id: 2,
+                answer: "will go",
+                isCorrect: false
+            },
+            {
+                id: 3,
+                answer: "are going to",
+                isCorrect: true
+            }
+        ] 
+    },
+    {
+        id: 4,
+        firstPart: 'She said she',
+        secondPart: 'help us with the project',
+        description: 'Нужно добавить объяснение',
+        answers: [
+            {
+                id: 1,
+                answer: 'are',
+                isCorrect: false
+            },
+            {
+                id: 2,
+                answer: "will",
+                isCorrect: true
+            },
+            {
+                id: 3,
+                answer: "is",
+                isCorrect: false
+            }
+        ] 
+    },
+    {
+        id: 5,
+        firstPart: "",
+        secondPart: 'they arrive on time for the meeting?',
+        description: 'Нужно добавить объяснение',
+        answers: [
+            {
+                id: 1,
+                answer: 'are',
+                isCorrect: false
+            },
+            {
+                id: 2,
+                answer: 'will',
+                isCorrect: true
+            },
+            {
+                id: 3,
+                answer: 'is',
+                isCorrect: false
+            }
+        ] 
+    },
+    {
+        id: 6,
+        firstPart: "He is",
+        secondPart: "learn to play chess this summer",
+        description: 'Нужно добавить объяснение',
+        answers: [
+            {
+                id: 1,
+                answer: "are",
+                isCorrect: false
+            },
+            {
+                id: 2,
+                answer: 'will',
+                isCorrect: true
+            },
+            {
+                id: 3,
+                answer: "going to",
+                isCorrect: false
+            }
+        ] 
+    },
+    {
+        id: 7,
+        firstPart: 'We are',
+        secondPart: 'visit the new museum exhibit this weekend',
+        description: 'Нужно добавить объяснение',
+        answers: [
+            {
+                id: 1,
+                answer: "are",
+                isCorrect: false
+            },
+            {
+                id: 2,
+                answer: "is",
+                isCorrect: false
+            },
+            {
+                id: 3,
+                answer: "going to",
+                isCorrect: true
+            }
+        ] 
+    },
+    {
+        id: 8,
+        firstPart: "By the time we get there, the match",
+        secondPart: "already start.",
+        description: 'Нужно добавить объяснение',
+        answers: [
+            {
+                id: 1,
+                answer: 'are',
+                isCorrect: false
+            },
+            {
+                id: 2,
+                answer: "will",
+                isCorrect: true
+            },
+            {
+                id: 3,
+                answer: 'is',
+                isCorrect: false
+            }
+        ] 
+    },
+    {
+        id: 9,
+        firstPart: "",
+        secondPart: 'you help me with my homework later?',
+        description: 'Нужно добавить объяснение',
+        answers: [
+            {
+                id: 1,
+                answer: "will",
+                isCorrect: true
+            },
+            {
+                id: 2,
+                answer: 'are',
+                isCorrect: false
+            },
+            {
+                id: 3,
+                answer: 'is',
+                isCorrect: false
+            }
+        ] 
+    },
+    {
+        id: 10,
+        firstPart: "The company",
+        secondPart: 'launch a new product next month',
+        description: 'Нужно добавить объяснение',
+        answers: [
+            {
+                id: 1,
+                answer: "are",
+                isCorrect: false
+            },
+            {
+                id: 2,
+                answer: "will",
+                isCorrect: true
+            },
+            {
+                id: 3,
+                answer: "going to",
+                isCorrect: false
+            }
+        ] 
+    }
+]
