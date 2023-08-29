ea8d8d141123c44a528a53b01c14508309d749f9
DIYAZ MUKHANOV <diyaz.mukhanov@gmail.com>
2023-08-29 22:15:32 +0400

    Lesson 24 test

---
+++ utils/lessonsMaterials/lesson22/test-materials.js
@@ -0,0 +1,232 @@
+export const questions = [
+    {
+        id: 1,
+        firstPart: "If I had more time I",
+        secondPart: 'learn to play basketball.',
+        description: 'Нужно добавить объяснение',
+        answers: [
+            {
+                id: 1,
+                answer: 'will',
+                isCorrect: false
+            },
+            {
+                id: 2,
+                answer: 'would',
+                isCorrect: true
+            },
+            {
+                id: 3,
+                answer: 'would be',
+                isCorrect: false
+            }
+        ] 
+    },
+    {
+        id: 2,
+        firstPart: "She said she",
+        secondPart: 'help if she could.',
+        description: 'Нужно добавить объяснение',
+        answers: [
+            {
+                id: 1,
+                answer: 'will',
+                isCorrect: false
+            },
+            {
+                id: 2,
+                answer: 'would',
+                isCorrect: true
+            },
+            {
+                id: 3,
+                answer: 'will be',
+                isCorrect: false
+            }
+        ] 
+    },
+    {
+        id: 3,
+        firstPart: 'If you need assistance I',
+        secondPart: 'happy to help',
+        description: 'Нужно добавить объяснение',
+        answers: [
+            {
+                id: 1,
+                answer: "will",
+                isCorrect: false
+            },
+            {
+                id: 2,
+                answer: "would",
+                isCorrect: false
+            },
+            {
+                id: 3,
+                answer: "will be",
+                isCorrect: true
+            }
+        ] 
+    },
+    {
+        id: 4,
+        firstPart: 'If he studied harder he',
+        secondPart: 'get better grades.',
+        description: 'Нужно добавить объяснение',
+        answers: [
+            {
+                id: 1,
+                answer: 'will',
+                isCorrect: true
+            },
+            {
+                id: 2,
+                answer: "would",
+                isCorrect: false
+            },
+            {
+                id: 3,
+                answer: "will be",
+                isCorrect: false
+            }
+        ] 
+    },
+    {
+        id: 5,
+        firstPart: "If she asked I",
+        secondPart: 'lend her my notes.',
+        description: 'Нужно добавить объяснение',
+        answers: [
+            {
+                id: 1,
+                answer: 'would',
+                isCorrect: true
+            },
+            {
+                id: 2,
+                answer: 'will be',
+                isCorrect: false
+            },
+            {
+                id: 3,
+                answer: 'would be',
+                isCorrect: false
+            }
+        ] 
+    },
+    {
+        id: 6,
+        firstPart: "If it rains tomorrow we",
+        secondPart: "have to postpone the picnic",
+        description: 'Нужно добавить объяснение',
+        answers: [
+            {
+                id: 1,
+                answer: "would",
+                isCorrect: false
+            },
+            {
+                id: 2,
+                answer: 'will',
+                isCorrect: true
+            },
+            {
+                id: 3,
+                answer: "would be",
+                isCorrect: false
+            }
+        ] 
+    },
+    {
+        id: 7,
+        firstPart: 'If I earn a lot of money I',
+        secondPart: 'travel the world',
+        description: 'Нужно добавить объяснение',
+        answers: [
+            {
+                id: 1,
+                answer: "will",
+                isCorrect: false
+            },
+            {
+                id: 2,
+                answer: "would",
+                isCorrect: true
+            },
+            {
+                id: 3,
+                answer: "will be",
+                isCorrect: false
+            }
+        ] 
+    },
+    {
+        id: 8,
+        firstPart: "She said she",
+        secondPart: "come to the wedding party if she had time.",
+        description: 'Нужно добавить объяснение',
+        answers: [
+            {
+                id: 1,
+                answer: 'will',
+                isCorrect: false
+            },
+            {
+                id: 2,
+                answer: "would",
+                isCorrect: true
+            },
+            {
+                id: 3,
+                answer: 'would be',
+                isCorrect: false
+            }
+        ] 
+    },
+    {
+        id: 9,
+        firstPart: "They",
+        secondPart: 'surprised if they knew the truth',
+        description: 'Нужно добавить объяснение',
+        answers: [
+            {
+                id: 1,
+                answer: "would",
+                isCorrect: false
+            },
+            {
+                id: 2,
+                answer: 'would be',
+                isCorrect: true
+            },
+            {
+                id: 3,
+                answer: 'will be',
+                isCorrect: false
+            }
+        ] 
+    },
+    {
+        id: 10,
+        firstPart: "If I were you I",
+        secondPart: 'accept that job offer',
+        description: 'Нужно добавить объяснение',
+        answers: [
+            {
+                id: 1,
+                answer: "will",
+                isCorrect: false
+            },
+            {
+                id: 2,
+                answer: "would",
+                isCorrect: true
+            },
+            {
+                id: 3,
+                answer: "will be",
+                isCorrect: false
+            }
+        ] 
+    }
+]
---
+++ utils/lessonsMaterials/lesson23/test-materials.js
@@ -0,0 +1,232 @@
+export const questions = [
+    {
+        id: 1,
+        firstPart: "She loves pizza, and he loves pizza",
+        secondPart: '.',
+        description: 'Нужно добавить объяснение',
+        answers: [
+            {
+                id: 1,
+                answer: 'also',
+                isCorrect: false
+            },
+            {
+                id: 2,
+                answer: 'too',
+                isCorrect: true
+            },
+            {
+                id: 3,
+                answer: 'neither',
+                isCorrect: false
+            }
+        ] 
+    },
+    {
+        id: 2,
+        firstPart: "Sarah wants to go to the movies, and Tom wants to go to the movies",
+        secondPart: '.',
+        description: 'Нужно добавить объяснение',
+        answers: [
+            {
+                id: 1,
+                answer: 'also',
+                isCorrect: false
+            },
+            {
+                id: 2,
+                answer: 'too',
+                isCorrect: true
+            },
+            {
+                id: 3,
+                answer: 'either',
+                isCorrect: false
+            }
+        ] 
+    },
+    {
+        id: 3,
+        firstPart: "I don't like horror movies, and my sister doesn't like horror movies",
+        secondPart: '.',
+        description: 'Нужно добавить объяснение',
+        answers: [
+            {
+                id: 1,
+                answer: "also",
+                isCorrect: false
+            },
+            {
+                id: 2,
+                answer: "too",
+                isCorrect: false
+            },
+            {
+                id: 3,
+                answer: "neither",
+                isCorrect: true
+            }
+        ] 
+    },
+    {
+        id: 4,
+        firstPart: "Sam can speak French, and Alex can speak French",
+        secondPart: '.',
+        description: 'Нужно добавить объяснение',
+        answers: [
+            {
+                id: 1,
+                answer: 'also',
+                isCorrect: false
+            },
+            {
+                id: 2,
+                answer: "too",
+                isCorrect: true
+            },
+            {
+                id: 3,
+                answer: "neither",
+                isCorrect: false
+            }
+        ] 
+    },
+    {
+        id: 5,
+        firstPart: "The weather is cold, and the wind is cold",
+        secondPart: '.',
+        description: 'Нужно добавить объяснение',
+        answers: [
+            {
+                id: 1,
+                answer: 'too',
+                isCorrect: true
+            },
+            {
+                id: 2,
+                answer: 'so',
+                isCorrect: false
+            },
+            {
+                id: 3,
+                answer: 'either',
+                isCorrect: false
+            }
+        ] 
+    },
+    {
+        id: 6,
+        firstPart: "Jenny is allergic to peanuts, and her brother is allergic to peanuts",
+        secondPart: ".",
+        description: 'Нужно добавить объяснение',
+        answers: [
+            {
+                id: 1,
+                answer: "neither",
+                isCorrect: false
+            },
+            {
+                id: 2,
+                answer: 'either',
+                isCorrect: true
+            },
+            {
+                id: 3,
+                answer: "too",
+                isCorrect: false
+            }
+        ] 
+    },
+    {
+        id: 7,
+        firstPart: "Susan didn't want to eat the cake, and Mark didn't want to eat the cake",
+        secondPart: '.',
+        description: 'Нужно добавить объяснение',
+        answers: [
+            {
+                id: 1,
+                answer: "neither",
+                isCorrect: true
+            },
+            {
+                id: 2,
+                answer: "too",
+                isCorrect: false
+            },
+            {
+                id: 3,
+                answer: "so",
+                isCorrect: false
+            }
+        ] 
+    },
+    {
+        id: 8,
+        firstPart: "The cat doesn't like water, and the dog doesn't like water",
+        secondPart: ".",
+        description: 'Нужно добавить объяснение',
+        answers: [
+            {
+                id: 1,
+                answer: 'neither',
+                isCorrect: true
+            },
+            {
+                id: 2,
+                answer: "so",
+                isCorrect: false
+            },
+            {
+                id: 3,
+                answer: 'either',
+                isCorrect: false
+            }
+        ] 
+    },
+    {
+        id: 9,
+        firstPart: "They are going to the stadium, and we are going to the stadium",
+        secondPart: '.',
+        description: 'Нужно добавить объяснение',
+        answers: [
+            {
+                id: 1,
+                answer: "either",
+                isCorrect: false
+            },
+            {
+                id: 2,
+                answer: 'too',
+                isCorrect: false
+            },
+            {
+                id: 3,
+                answer: 'so',
+                isCorrect: true
+            }
+        ] 
+    },
+    {
+        id: 10,
+        firstPart: "I can't swim, and my friend can't swim",
+        secondPart: '.',
+        description: 'Нужно добавить объяснение',
+        answers: [
+            {
+                id: 1,
+                answer: "neither",
+                isCorrect: true
+            },
+            {
+                id: 2,
+                answer: "either",
+                isCorrect: false
+            },
+            {
+                id: 3,
+                answer: "so",
+                isCorrect: false
+            }
+        ] 
+    }
+]
