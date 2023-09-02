1c3d6922fb82209fc04a42503d054f0080a01caf
DIYAZ MUKHANOV <diyaz.mukhanov@gmail.com>
2023-09-02 14:38:28 +0400

    yandex

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -1,4 +1,5 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+import Script from 'next/script'
  
 export default function Document() {
   return (
@@ -7,30 +8,29 @@ export default function Document() {
       <link rel="preconnect" href="https://fonts.googleapis.com" />
       <link crossOrigin="true" rel="preconnect" href="https://fonts.gstatic.com" />
       <link href="https://fonts.googleapis.com/css2?family=Inter&display=swap" rel="stylesheet" />
-      {
-         <script dangerouslySetInnerHTML={ {
-          __html: `(function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
-          m[i].l=1*new Date();
-          for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
-          k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
-          (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
-       
-          ym(94815906, "init", {
-               clickmap:true,
-               trackLinks:true,
-               accurateTrackBounce:true
-          }););`,
-      } }
-/>
-      }
+      <Script
+      id='yandex'
+      strategy="beforeInteractive"
+        >
+      {`  (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+   m[i].l=1*new Date();
+   for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+   k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+   (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
 
-      {<noscript>
-        <div><img src="https://mc.yandex.ru/watch/94815906" style={{ position: 'absolute', left: '-9999px' }} alt="" /></div>
-    </noscript>
-      }
+   ym(94815906, "init", {
+        clickmap:true,
+        trackLinks:true,
+        accurateTrackBounce:true
+   });`}  
+      </Script>
         </Head>
       <body>
         <Main />
+        {<noscript>
+        <div><img src="https://mc.yandex.ru/watch/94815906" style={{ position: 'absolute', left: '-9999px' }} alt="" /></div>
+        </noscript>
+      }
         <NextScript />
       </body>
     </Html>
