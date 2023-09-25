35c6ab75630a237da51bf03232260d085d2ee31c
DIYAZ MUKHANOV <diyaz.mukhanov@gmail.com>
2023-09-25 13:22:34 +0400

    cards lesson

--- lessons/beginner/lesson1.jsx
+++ lessons/beginner/lesson1.jsx
@@ -7,72 +7,72 @@ export default function BeginnerLesson1() {
       {
         ru: 'яблоко',
         enRight: 'apple',
-        imageUrl: '/images/food/apple.jpg',
+        imageUrl: 'https://storage.googleapis.com/langy.su/images/food/apple.jpg',
         enWrong: ['orange', 'bread'],
-        audioSrc: '/audio/lesson1/apple.mp3'
+        audioSrc: 'https://storage.googleapis.com/langy.su/audio/lesson1/apple.mp3'
       },
       {
           ru: 'хлеб',
           enRight: 'bread',
-          imageUrl: '/images/food/bread.jpg',
+          imageUrl: 'https://storage.googleapis.com/langy.su/images/food/bread.jpg',
           enWrong: ['cheese', 'tomato'],
-          audioSrc: '/audio/lesson1/bread.mp3'
+          audioSrc: 'https://storage.googleapis.com/langy.su/audio/lesson1/bread.mp3'
         },
         {
           ru: 'сыр',
           enRight: 'cheese',
-          imageUrl: '/images/food/cheese.jpg',
+          imageUrl: 'https://storage.googleapis.com/langy.su/images/food/cheese.jpg',
           enWrong: ['ice cream', 'fish'],
-          audioSrc: '/audio/lesson1/cheese.mp3'
+          audioSrc: 'https://storage.googleapis.com/langy.su/audio/lesson1/cheese.mp3'
         },
         {
           ru: 'помидор',
           enRight: 'tomato',
-          imageUrl: '/images/food/tomato.jpg',
+          imageUrl: 'https://storage.googleapis.com/langy.su/images/food/tomato.jpg',
           enWrong: ['potato', 'chicken'],
-          audioSrc: '/audio/lesson1/tomato.mp3'
+          audioSrc: 'https://storage.googleapis.com/langy.su/audio/lesson1/tomato.mp3'
         },
         {
           ru: 'картофель',
           enRight: 'potato',
-          imageUrl: '/images/food/potato.jpg',
+          imageUrl: 'https://storage.googleapis.com/langy.su/images/food/potato.jpg',
           enWrong: ['soup', 'chocolate'],
-          audioSrc: '/audio/lesson1/potato.mp3'
+          audioSrc: 'https://storage.googleapis.com/langy.su/audio/lesson1/potato.mp3'
         },
         {
           ru: 'рыба',
           enRight: 'fish',
-          imageUrl: '/images/food/fish.jpg',
+          imageUrl: 'https://storage.googleapis.com/langy.su/images/food/fish.jpg',
           enWrong: ['apple', 'bread'],
-          audioSrc: '/audio/lesson1/fish.mp3'
+          audioSrc: 'https://storage.googleapis.com/langy.su/audio/lesson1/fish.mp3'
         },
         {
           ru: 'курица',
           enRight: 'chicken',
-          imageUrl: '/images/food/chicken.jpg',
+          imageUrl: 'https://storage.googleapis.com/langy.su/images/food/chicken.jpg',
           enWrong: ['tomato', 'tea'],
-          audioSrc: '/audio/lesson1/chicken.mp3'
+          audioSrc: 'https://storage.googleapis.com/langy.su/audio/lesson1/chicken.mp3'
         },
         {
           ru: 'суп',
           enRight: 'soup',
-          imageUrl: '/images/food/soup.jpg',
+          imageUrl: 'https://storage.googleapis.com/langy.su/images/food/soup.jpg',
           enWrong: ['coffee', 'cheese'],
-          audioSrc: '/audio/lesson1/soup.mp3'
+          audioSrc: 'https://storage.googleapis.com/langy.su/audio/lesson1/soup.mp3'
         },
         {
           ru: 'шоколад',
           enRight: 'chocolate',
-          imageUrl: '/images/food/chocolate.jpg',
+          imageUrl: 'https://storage.googleapis.com/langy.su/images/food/chocolate.jpg',
           enWrong: ['tomato', 'orange'],
-          audioSrc: '/audio/lesson1/chocolate.mp3'
+          audioSrc: 'https://storage.googleapis.com/langy.su/audio/lesson1/chocolate.mp3'
         },
         {
           ru: 'мороженое',
           enRight: 'ice cream',
-          imageUrl: '/images/food/ice_cream.jpg',
+          imageUrl: 'https://storage.googleapis.com/langy.su/images/food/ice_cream.jpg',
           enWrong: ['potato', 'fish'],
-          audioSrc: '/audio/lesson1/ice_cream.mp3'
+          audioSrc: 'https://storage.googleapis.com/langy.su/audio/lesson1/ice_cream.mp3'
         }
     ];
    
