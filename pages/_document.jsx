7295e95b11e82051a16aa396740b9d89dcf43e2b
DIYAZ MUKHANOV <diyaz.mukhanov@gmail.com>
2023-09-26 13:08:06 +0400

    removed google metrik

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -31,21 +31,6 @@ export default function Document() {
           </Script>
       )}
 
-      {process.env.NEXT_PUBLIC_ENVIRONMENT === `production` && (
-          <Script
-          async src="https://www.googletagmanager.com/gtag/js?id=G-BD995SMJR5"
-            ></Script>
-      )}
-      {process.env.NEXT_PUBLIC_ENVIRONMENT === `production` && (
-          <Script>
-           { `window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-
-            gtag('config', 'G-BD995SMJR5')`}
-          </Script>
-      )}			
-     
         </Head>
       <body>
         <Main />
