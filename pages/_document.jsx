5c0c7656e2dc43be860e82094822914b9dd4768f
DIYAZ MUKHANOV <diyaz.mukhanov@gmail.com>
2023-10-29 15:44:13 +0400

    prep for production

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -15,12 +15,12 @@ export default function Document() {
       <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,300;0,400;0,500;0,600;1,300;1,600&family=Roboto:wght@500;700&display=swap" rel="stylesheet" /> */}
         <meta name="description" content="Уроки английского языка онлайн" />
         <meta name="описание" content="Онлайн уроки английского языка" />
-        {/* {process.env.NEXT_PUBLIC_ENVIRONMENT === `production` && (
+        {process.env.NEXT_PUBLIC_ENVIRONMENT === `production` && (
           <Script
-          id='yandex'
-          strategy="beforeInteractive"
-            >
-          {`  (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+            id='yandex'
+            strategy="beforeInteractive"
+          >
+            {`  (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
           m[i].l=1*new Date();
           for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
           k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
@@ -30,9 +30,9 @@ export default function Document() {
             clickmap:true,
             trackLinks:true,
             accurateTrackBounce:true
-          });`}  
+          });`}
           </Script>
-      )} */}
+        )}
 
       </Head>
       <body>
