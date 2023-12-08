6a47deef2b31f3df24bc4af49a10c82e762f8601
DIYAZ MUKHANOV <diyaz.mukhanov@gmail.com>
2023-12-08 14:27:57 +0400

    Everyday progress

--- modules/study/lessons/advanced/test.jsx
+++ modules/study/lessons/advanced/test.jsx
@@ -1,5 +1,4 @@
 import TestLessonLayout from "@/modules/study/advanced/test/TestLessonLayout";
-import { questions } from "@/utils/lessonsMaterials/lesson1/test-materials";
 import { nextLessonUrlGenerator } from "../shared/nextLessonUrlGenerator";
 import { getLevel } from "../utils/getLevel";
 import { useRouter } from "next/router";
