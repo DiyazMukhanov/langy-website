6c73801c2a67a14553fa06d5ad6334622eeb3cd7
DIYAZ MUKHANOV <diyaz.mukhanov@gmail.com>
2023-12-12 10:22:50 +0400

    everyday progress

--- modules/study/lessons/everydayEnglish/phrasesLesson.tsx
+++ modules/study/lessons/everydayEnglish/phrasesLesson.tsx
@@ -3,14 +3,23 @@ import PhrasesLessonLayout from "../../everydayEnglish/PhrasesLessonLayout"
 import { CHAPTER_1 } from "../../everydayEnglish/shared/navRoutes"
 import { phrases } from "@/utils/lessonsMaterials/easyEnglish/lesson1/phrases"
 import EveryDayEnglishLayout from "../../everydayEnglish/shared/EverydayEnglishLayout/EverydayEnglishLayout"
+import { useLessonMaterials } from "../../shared/hooks/useLessonMaterials"
+import { getPhrases } from "./shared/api/getPhrases"
+import Loader from "@/modules/shared/Loader"
 
 export default function PhrasesLesson() {
     const router = useRouter()
     const lessonNumber = Number(router.query.number)
+    const { isLoading, lessonMaterials } = useLessonMaterials(getPhrases, lessonNumber)
 
-    return <>
-        <EveryDayEnglishLayout lessonNumber={lessonNumber} chapter={CHAPTER_1}>
-            <PhrasesLessonLayout data={phrases} />
-        </EveryDayEnglishLayout>
-    </>
+    if (isLoading) {
+        return <Loader small={false} />
+    } else {
+        return <>
+            <EveryDayEnglishLayout lessonNumber={lessonNumber} chapter={CHAPTER_1}>
+                <PhrasesLessonLayout data={lessonMaterials.data} />
+            </EveryDayEnglishLayout>
+        </>
+    }
+
 }
