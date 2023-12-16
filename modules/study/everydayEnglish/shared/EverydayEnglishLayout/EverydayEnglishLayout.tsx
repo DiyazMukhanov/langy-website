d9d6240af57c8aaadd21fcf0868775dea9a28b00
DIYAZ MUKHANOV <diyaz.mukhanov@gmail.com>
2023-12-16 08:05:20 +0400

    Исправил положение кнопки далее

--- modules/study/everydayEnglish/shared/EverydayEnglishLayout/EverydayEnglishLayout.tsx
+++ modules/study/everydayEnglish/shared/EverydayEnglishLayout/EverydayEnglishLayout.tsx
@@ -6,7 +6,6 @@ import EverydayEnglishNav from "../EverydayEnglishNav/index"
 import EverydayEnglishNextButtonSection from "../EverydayEnglishNextButtonSection/index"
 import { setCurrentLearningField } from "../../../shared/api/setCurrentLearningField"
 import { updateEverydayProgress } from "../api/updateEverydayProgress"
-import classNames from "classnames"
 
 type Props = {
     lessonNumber: number
@@ -41,19 +40,16 @@ export default function EveryDayEnglishLayout({ lessonNumber, children, chapter 
             adminNeeded={false}
         >
             <Header variant='white' isEasyEnglish={true} />
-            <div className={classNames(
-                styles.container,
-                { [styles.dialogue]: chapter === 'dialogue' }
-            )}>
-                <div className={styles.top}>
-                    <div className={styles.navContainer}>
-                        <EverydayEnglishNav lessonNumber={lessonNumber} />
-                    </div>
+            <div className={styles.top}>
+                <div className={styles.navContainer}>
+                    <EverydayEnglishNav lessonNumber={lessonNumber} />
+                </div>
+                <div className={styles.rightBlock}>
                     <div className={styles.mainSectionContainer}>
                         {children}
                     </div>
+                    <EverydayEnglishNextButtonSection lessonNumber={lessonNumber} chapter={chapter} />
                 </div>
-                <EverydayEnglishNextButtonSection lessonNumber={lessonNumber} chapter={chapter} />
             </div>
         </ProtectPage>
     </>
