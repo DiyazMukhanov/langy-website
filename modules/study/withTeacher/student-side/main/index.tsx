2f482f0aa5e8a9cb302082e7c5a1b81f9a16f09c
DIYAZ MUKHANOV <diyaz.mukhanov@gmail.com>
2024-01-27 15:54:09 +0400

    rebase with main

--- modules/study/withTeacher/student-side/main/index.tsx
+++ modules/study/withTeacher/student-side/main/index.tsx
@@ -3,6 +3,8 @@ import { Button } from "@/ui-kit/Button";
 import styles from './Main.module.scss';
 import { Card } from "@/ui-kit/Card";
 import { Paper } from "@/ui-kit/Paper";
+import Image from 'next/image';
+import Teacher from './../../../../../public/images/teacher.png';
 
 export default function Main() {
     return <>
@@ -11,8 +13,22 @@ export default function Main() {
             <Button variant="standardMiddleOutlined">Мой прогресс</Button>
             <Card className={styles.lessonsStatus}>
                 <Paper>Пополнить уроки</Paper>
-                <Paper>12 уроков</Paper>
+                <p>12 уроков</p>
             </Card>
+        </div>
+        <div className={styles.main}>
+            <Image
+                priority
+                src={Teacher}
+                className={styles.image}
+                alt="teacher"
+            />
+            <Button variant="teachers" className={styles.chooseTeacherBtn}>Выбрать преподавателя</Button>
+        </div>
+
+        <div className={styles.bottom}>
+            <Button variant="standardMiddleOutlined">Правила возврата уроков</Button>
+            <Button variant="standardMiddleOutlined">Правила отмены уроков</Button>
         </div>
     </>
 }
