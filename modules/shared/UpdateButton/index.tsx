e73802767d3dcea0250331aca7ad9f49662501cb
DIYAZ MUKHANOV <diyaz.mukhanov@gmail.com>
2024-08-15 17:10:02 +0400

    Прогресс

--- modules/shared/UpdateButton/index.tsx
+++ modules/shared/UpdateButton/index.tsx
@@ -3,5 +3,10 @@ import styles from "./UpdateButton.module.scss";
 import Update from "../../../public/images/update.svg";
 
 export default function UpdateButton({ onClick }) {
-  return <Update onClick={onClick} className={styles.btn} />;
+  return (
+    <div className={styles.updateContainer} onClick={onClick}>
+      <span>Ещё</span>
+      <Update onClick={onClick} className={styles.btn} />
+    </div>
+  );
 }
--- modules/study/everydayEnglish/DialogueLessonLayout/index.tsx
+++ modules/study/everydayEnglish/DialogueLessonLayout/index.tsx
@@ -1,22 +1,35 @@
-import EverydayEnglishContainer from '../shared/EverydayEnglishContainer'
-import Audioplayer from './../../../shared/Audioplayer/index'
-import styles from './DialogueLessonLayout.module.scss'
+import EverydayEnglishContainer from "../shared/EverydayEnglishContainer";
+import Audioplayer from "./../../../shared/Audioplayer/index";
+import styles from "./DialogueLessonLayout.module.scss";
+import Image from "next/image";
+import TwoWomen from "../../../../public/images/two_women.webp";
 
 export default function DialogueLessonLayout({ data }) {
-
-    return (
-        <div className={styles.container}>
-            <div className={styles.audioContainer}>
-                <Audioplayer audioSrc={data.src} />
-            </div>
-            <EverydayEnglishContainer className={styles.internalLayout}>
-                {data?.dialogue.map(item => (
-                    <div key={item.id}>
-                        <p className={styles.en}>{item.nameEn}: {item.phraseEn}</p>
-                        <p className={styles.ru}>{item.nameRu}: {item.phraseRu}</p>
-                    </div>
-                ))}
-            </EverydayEnglishContainer>
-        </div>
-    )
+  return (
+    <div className={styles.container}>
+      <div className={styles.audioContainer}>
+        <Audioplayer audioSrc={data.src} />
+      </div>
+      <h1 className={styles.h1}>Прослушайте диалог</h1>
+      <Image
+        src={TwoWomen}
+        alt="two women"
+        height={300}
+        style={{ borderRadius: "10px" }}
+        className={styles.image}
+      />
+      <EverydayEnglishContainer className={styles.internalLayout}>
+        {data?.dialogue.map((item) => (
+          <div key={item.id}>
+            <p className={styles.en}>
+              {item.nameEn}: {item.phraseEn}
+            </p>
+            <p className={styles.ru}>
+              {item.nameRu}: {item.phraseRu}
+            </p>
+          </div>
+        ))}
+      </EverydayEnglishContainer>
+    </div>
+  );
 }
