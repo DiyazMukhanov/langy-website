49810561ad23f378469103d57ffe4502176099e6
DIYAZ MUKHANOV <diyaz.mukhanov@gmail.com>
2024-10-02 12:43:08 +0400

    prod

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -1,15 +1,17 @@
-import { Html, Head, Main, NextScript } from 'next/document'
-import Script from 'next/script'
+import { Html, Head, Main, NextScript } from "next/document";
+import Script from "next/script";
 
 export default function Document() {
-
   return (
     <Html lang="en">
       <Head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" />
         {/* <link rel='manifest' href="/manifest.json" /> */}
-        <link href="https://fonts.googleapis.com/css2?family=AR+One+Sans:wght@400;500;600;700&family=Montserrat:ital,wght@0,300;0,400;0,500;0,600;1,300;1,600&family=Roboto:wght@500;700&display=swap" rel="stylesheet" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=AR+One+Sans:wght@400;500;600;700&family=Montserrat:ital,wght@0,300;0,400;0,500;0,600;1,300;1,600&family=Roboto:wght@500;700&display=swap"
+          rel="stylesheet"
+        />
         {/* <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link crossOrigin="true" rel="preconnect" href="https://fonts.gstatic.com" />
         <link href="https://fonts.googleapis.com/css2?family=Inter&display=swap" rel="stylesheet" /> */}
@@ -19,11 +21,8 @@ export default function Document() {
       <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,300;0,400;0,500;0,600;1,300;1,600&family=Roboto:wght@500;700&display=swap" rel="stylesheet" /> */}
         <meta name="description" content="Уроки английского языка онлайн" />
         <meta name="описание" content="Онлайн уроки английского языка" />
-        {/* {process.env.NEXT_PUBLIC_ENVIRONMENT === `production` && (
-          <Script
-            id='yandex'
-            strategy="beforeInteractive"
-          >
+        {process.env.NEXT_PUBLIC_ENVIRONMENT === `production` && (
+          <Script id="yandex" strategy="beforeInteractive">
             {`  (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
           m[i].l=1*new Date();
           for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
@@ -36,8 +35,7 @@ export default function Document() {
             accurateTrackBounce:true
           });`}
           </Script>
-        )} */}
-
+        )}
       </Head>
       <body>
         <Main />
@@ -48,6 +46,5 @@ export default function Document() {
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
-
